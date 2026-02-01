847306dd1835f68f7cd19de82b2f557b97106143
Harish9199 <thorathharish9@gmail.com>
2026-02-01 17:28:32 +0530

    Using get() and if else methods

--- strings/count_char_frequency.py
+++ strings/count_char_frequency.py
@@ -10,10 +10,22 @@ Approach:
 Time Complexity: O(n)
 """
 
+
+# Method1 --> Using get() 
 s = "aabcaba"
 freq = {}
 
 for ch in s:
     freq[ch] = freq.get(ch, 0) + 1
 
+print(freq)
+
+# Method2 --> Using if-else
+s = "aabcaba"
+freq = {}
+for ch in s:
+    if ch in freq:
+        freq[ch] += 1
+    else:
+        freq[ch] = 1
 print(freq)
